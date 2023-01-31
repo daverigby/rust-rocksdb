bab33dc4d3ff700c162a3f12c2b02c79631d2e17
Arthur Silva <arthurprs@gmail.com>
2023-01-31 18:34:50 +0100

    Expose Iterator::Refresh

--- librocksdb_sys/src/lib.rs
+++ librocksdb_sys/src/lib.rs
@@ -1353,6 +1353,7 @@ extern "C" {
     pub fn crocksdb_iter_key(iter: *const DBIterator, klen: *mut size_t) -> *mut u8;
     pub fn crocksdb_iter_value(iter: *const DBIterator, vlen: *mut size_t) -> *mut u8;
     pub fn crocksdb_iter_get_error(iter: *const DBIterator, err: *mut *mut c_char);
+    pub fn crocksdb_iter_refresh(iter: *const DBIterator, err: *mut *mut c_char);
     // Write batch
     pub fn crocksdb_write(
         db: *mut DBInstance,
